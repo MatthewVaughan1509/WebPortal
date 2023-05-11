e3706b9f19bdc21c00afccf3d3280e7e9b4e012e
Vaughan,Matthew <VAUGHAM2@airproducts.com>
2023-05-11 12:51:12 +0100

    Page details.

--- WebPortal/src/app/pages/page-details/page-details.component.ts
+++ WebPortal/src/app/pages/page-details/page-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PageDetailsService } from './page-details.service';
+import { PageDetails } from 'src/app/entities/page-details';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-page-details',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageDetailsComponent implements OnInit {
 
-  constructor() { }
+  pageId: string;
+  pageDetails: PageDetails;
+  hasEditPermission: boolean = false;
+  title: string = "Edit Page";
+  action: string = "Edit";
+  useSubmitBehavior: boolean = true;
+
+  constructor(private route: ActivatedRoute, private service: PageDetailsService) {
+    this.loadForm();
+  }
+
+  loadForm() {
+    this.pageId = this.route.snapshot.queryParamMap.get('id');
+    if (this.pageId) {
+      forkJoin([
+        this.service.getPageById(this.pageId)
+      ]).subscribe(results => {
+        if (results && results[0]) {
+          this.pageDetails = results[0];
+        }
+      }
+      );
+    }
+    this.hasEditPermission = true; // This needs to be set from a permissions API.
+  }
 
   ngOnInit(): void {
   }
 
+  submit(e: any) {
+    alert("Submit");
+  }
+
+  cancel() {
+    window.close();
+  }
 }
--- WebPortal/src/app/pages/page-details/page-details.service.ts
+++ WebPortal/src/app/pages/page-details/page-details.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { PageDetails } from 'src/app/entities/page-details';
+import { AppSetings, AppSettingsService } from 'src/app/shared/services/app-settings.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PageDetailsService {
 
-  constructor() { }
+  baseUrl: string;
+
+  constructor(private http: HttpClient, private appSettingsService: AppSettingsService) { 
+    let appSettings: AppSetings = this.appSettingsService.readSettings();
+    this.baseUrl = appSettings.baseUrl;
+  }
+
+  getPageById(id: string): Observable<PageDetails> {
+    return this.http.get<PageDetails>(`${this.baseUrl}Page/GetPageById/${id}`)
+    .pipe(catchError(this.handleError));
+  }
+
+  private handleError(err: any) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      //Client Side Error
+      errorMessage = `Error : ${err.error.message}`;
+    } else {
+      //Server Side Error
+      errorMessage = `Message : ${err.message}\nError : ${err.error}`;
+    }
+    console.log(errorMessage);
+    //this is to display error/api exception message to the user
+    return throwError(errorMessage);
+  }
 }
